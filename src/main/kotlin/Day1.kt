593c278df49f64b24900b3141ee07cf179cdd015
sellithy <shehab.ellithy@gmail.com>
2021-12-01 22:21:10 -0800

    Day 1

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+fun main() {
+    println("Part 1: ${part1()}")
+    println("Part 1: ${part2()}")
+}
+
+private fun part1() =
+    File(ClassLoader.getSystemResource("inputDay1").file).useLines { lines ->
+        lines.map(String::toInt)
+            .zipWithNext { a, b -> b - a }
+            .count { it > 0 }
+    }
+
+private fun part2() =
+    File(ClassLoader.getSystemResource("inputDay1").file).useLines { lines ->
+        lines.map(String::toInt)
+            .windowed(3) { it.sum() }
+            .zipWithNext() { a, b ->  b - a}
+            .count { it > 0 }
+    }
