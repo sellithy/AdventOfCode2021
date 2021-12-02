6438afc3ed0ec9b732d38049c1ec071cee340798
sellithy <shehab.ellithy@gmail.com>
2021-12-01 23:02:33 -0800

    Day 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -5,17 +5,41 @@ fun main() {
     println("Part 2: ${part2()}")
 }
 
+enum class Command {
+    Forward, Down, Up
+}
+
 private fun part1() =
-    File(ClassLoader.getSystemResource("inputDay1").file).useLines { lines ->
-        lines.map(String::toInt)
-            .zipWithNext { a, b -> b - a }
-            .count { it > 0 }
+    File(ClassLoader.getSystemResource("inputDay2").file).useLines { lines ->
+        lines.fold(0 to 0) { (horizontal, depth), line ->
+            val (command, amount) = parseLine(line)
+
+            when (command) {
+                Command.Forward -> horizontal + amount to depth
+                Command.Down -> horizontal to depth + amount
+                Command.Up -> horizontal to depth - amount
+            }
+        }.let { (horizontal, depth) -> horizontal * depth }
     }
 
 private fun part2() =
-    File(ClassLoader.getSystemResource("inputDay1").file).useLines { lines ->
-        lines.map(String::toInt)
-            .windowed(3) { it.sum() }
-            .zipWithNext() { a, b ->  b - a}
-            .count { it > 0 }
+    File(ClassLoader.getSystemResource("inputDay2").file).useLines { lines ->
+        lines.fold(0 to 0 tto 0) { (horizontal, depth, aim), line ->
+
+            val (command, amount) = parseLine(line)
+
+            when(command){
+                Command.Down -> horizontal to depth tto aim + amount
+                Command.Up -> horizontal to depth tto aim - amount
+                Command.Forward -> horizontal + amount to depth + aim * amount tto aim
+            }
+        }.let { (horizontal, depth, _) -> horizontal * depth }
     }
+
+private fun parseLine(line: String) =
+    line.split(' ').let { (command, amount) -> Command.valueOf(command.uppercaseFirst()) to amount.toInt() }
+
+private infix fun<A,B,C> Pair<A,B>.tto(c: C) = Triple(first, second, c)
+
+private fun String.uppercaseFirst() =
+    "${this[0].uppercase()}${substring(1)}"
